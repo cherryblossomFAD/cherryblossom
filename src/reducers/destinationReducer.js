141da79b8b885854ce68592de84d26d0ce8ec295
Wilhoite Frances, U-8US <Frances.Wilhoite@bmwna.com>
2016-03-28 06:23:41 -0500

    Added reducer for removing an item. add configureStore that uses reducers

--- src/reducers/destinationReducer.js
+++ src/reducers/destinationReducer.js
@@ -5,3 +5,21 @@ const {
 const initialState = {
   destinationList: []
 }
+
+module.exports = function destinationReducer(state = initialState, action) {
+  let list
+
+  case REMOVE_ITEM:
+    list = state.destinationList.slice(0)
+    const index = list.map(i => i.id).indexOf(action.id)
+    list.splice(index, 1)
+
+    return {
+      ...state,
+      destinationList: list
+    }
+
+  default:
+    return state
+  }
+}
