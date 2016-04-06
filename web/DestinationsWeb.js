c6a8c37c0e420b0143b801a67c7baf5f8dedb51b
Colon Anthony, US-204 <anthony.colon@bmwna.com>
2016-04-05 22:13:10 -0500

    Add nav bar and list group styling.

--- web/DestinationsWeb.js
+++ web/DestinationsWeb.js
@@ -2,6 +2,7 @@ var Destinations = React.createClass({
   render: function() {
     var createDestination = function(destination) {
       return (
+        <li className="list-group-item">
         <div className="media">
           <div className="media-left">
               <span className="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
@@ -11,10 +12,11 @@ var Destinations = React.createClass({
             { destination.address }
           </div>
         </div>
+        </li>
       );
     };
 
-    return <div>{ this.props.list.map(createDestination) }</div>
+    return <ul className="list-group">{ this.props.list.map(createDestination) }</ul>
   }
 });
 
