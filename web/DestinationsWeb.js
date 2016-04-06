440aa90de60954f52bc52abe02c60658c70b3e99
Colon Anthony, US-204 <anthony.colon@bmwna.com>
2016-04-05 21:11:10 -0500

    Updated styling with bootstrap

--- web/DestinationsWeb.js
+++ web/DestinationsWeb.js
@@ -2,12 +2,19 @@ var Destinations = React.createClass({
   render: function() {
     var createDestination = function(destination) {
       return (
-        <li>
-          { destination.title } <br /> {destination.address }
-        </li>
+        <div className="media">
+          <div className="media-left">
+              <span className="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+          </div>
+          <div className="media-body">
+            <h4 className="media-heading">{ destination.title }</h4>
+            { destination.address }
+          </div>
+        </div>
       );
     };
-    return <ul>{ this.props.list.map(createDestination) }</ul>;
+
+    return <div>{ this.props.list.map(createDestination) }</div>
   }
 });
 
